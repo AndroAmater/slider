7b41bab8523d0abf719f57773eedaf13ce81d3ca
Andrej Fidel <andrej@hudlajf.com>
2023-04-29 17:41:11 +0000

    chore: Remove all but one circles and readjust weights

--- slider.js
+++ slider.js
@@ -1,28 +1,20 @@
 function calculateDashWidth(r) {
     const C = 2 * Math.PI * r
-    let bestD = 6.4;
-    let bestDError = 7;
-    let currentD = 6.4;
-    let options = {}
-    while (currentD >= 5.9) {
-        if (C % (currentD + 2.2) < bestDError) {
+    let bestD = 6.7;
+    let bestDError = 9999;
+    let currentD = 6.7;
+    while (currentD >= 5.7) {
+        if (C % (currentD + 2) < bestDError) {
             bestD = currentD
-            bestDError = C % (currentD + 2.2) 
+            bestDError = C % (currentD + 2)
         }
         currentD = currentD - 0.01
     }
 
+    console.log(bestD)
     return bestD
 }
 
-const circle0 = document.getElementById("circle0")
-const circle1 = document.getElementById("circle1")
-const circle2 = document.getElementById("circle2")
-const circle3 = document.getElementById("circle3")
-const circle4 = document.getElementById("circle4")
+const circle = document.getElementById("circle")
 
-circle0.style['stroke-dasharray'] = `${calculateDashWidth(51)}px, 2.2px`
-circle1.style['stroke-dasharray'] = `${calculateDashWidth(84)}px, 2.2px`
-circle2.style['stroke-dasharray'] = `${calculateDashWidth(116)}px, 2.2px`
-circle3.style['stroke-dasharray'] = `${calculateDashWidth(148)}px, 2.2px`
-circle4.style['stroke-dasharray'] = `${calculateDashWidth(182)}px, 2.2px`
+circle.style['stroke-dasharray'] = `${calculateDashWidth(148)}px, 2px`
